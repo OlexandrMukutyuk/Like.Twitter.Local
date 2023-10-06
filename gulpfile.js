463ab8c5522391de25e7c63117120d5919a9d1f6
Oleksandr Mukutyuk <sasawa200@gmail.com>
2023-10-07 00:41:36 +0300

    Start Project

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+
+function scss() {
+    return src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions']
+        }))
+        .pipe(csso())
+        .pipe(concat('index.css'))
+        .pipe(dest('static/css'));
+}
+
+function build() {
+    return scss();
+}
+
+exports.build = build;
