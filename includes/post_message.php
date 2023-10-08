d6036648596587083462a5f6f76664c9774b44b2
Oleksandr Mukutyuk <sasawa200@gmail.com>
2023-10-08 22:24:20 +0300

    end main page creating post to database and output data into page

--- includes/post_message.php
+++ includes/post_message.php
@@ -1,10 +1,12 @@
 <?php
-print_r($_POST);
 session_start();
 require_once 'connect.php';
-
-if(!mysqli_query($connections, "INSERT INTO `post_message`(`id`, `user_id`, `message`) VALUES (NULL,'$_POST[user_id]','$_POST[post_message_t]')")){
+$date = date('Y-m-d H:i:s');
+echo $date;
+if(!mysqli_query($connections, "INSERT INTO `post_message` (`id`, `user_id`, `message`, `time`) VALUES (NULL, '$_POST[user_id]', '$_POST[post_message_t]', '$date')")){
     $_SESSION['post_message'] = "Незмогли добавити повідомлення у базу даних";
 }
-$_SESSION['post_message'] = "Успішно зроблений пост";
+else{
+    $_SESSION['post_message'] = "Успішно зроблений пост";
+}
 header('Location: ../make_post.php');
