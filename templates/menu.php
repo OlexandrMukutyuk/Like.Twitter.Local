22a6982352e297120d1cf9510883ef15902be08c
Oleksandr Mukutyuk <sasawa200@gmail.com>
2023-10-09 00:22:29 +0300

    added 3 icon

--- templates/menu.php
+++ templates/menu.php
@@ -8,9 +8,9 @@ session_start();
         <div class="sidebar-content">
             <img id="home_img" src="static/img/twitter_logo.png">
             <ul>
-                <li id="home"><img src="static/img/twitter_logo.png"><a>Головна</a></li>
-                <li id="profile"><img src="static/img/twitter_logo.png"><a>Профіль</a></li>
-                <li id="make-post"><img src="static/img/twitter_logo.png"><a>Зробити Пост</a></li>
+                <li id="home"><img src="static/img/home.png"><a>Головна</a></li>
+                <li id="profile"><img src="static/img/people.png"><a>Профіль</a></li>
+                <li id="make-post"><img src="static/img/mail.png"><a>Зробити Пост</a></li>
                 <li id="user-info">
                     <div class="item">
                         <img src="static/img/twitter_logo.png">
